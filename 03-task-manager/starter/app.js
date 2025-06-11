27965e9302cc48855dbd225cb6c3d986f5329472
jeyavidhya-web <jeyavidhya.mahesh@gmail.com>
2025-06-11 17:42:16 -0500

    week6

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,44 +1,33 @@
-require('dotenv').config();
 const express = require('express');
 const app = express();
+const tasks = require('./routes/Task');
+const connectDB = require('./db/connect');
+require('dotenv').config();
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
+// middleware
 
-const peopleRoutes = require('./routes/people');
-
-
-
-const mongoose = require('mongoose');
-
+app.use(express.static('./public'));
 app.use(express.json());
-app.use('/people', peopleRoutes);
 
-app.get('/people', (req, res) => {
-  res.send(people);
-});
- // Loads the .env file
-app.get('/people/:id', (req, res) => {
-  try{
-    const person = people.find(p => p.id === parseInt(req.params.id)); // Using 'id' as a URL parameter
-    if (!person) return res.status(404).send('Person not found');
-    res.send(person);
-}catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-});
-const PORT = process.env.PORT || 3000;
-const start =async() => {
+// routes
+
+app.use('/api/v1/tasks', tasks);
+
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+const port = process.env.PORT || 5000;
+
+const start = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-    console.log('Connected to MongoDB ✅');
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-  });
-}catch (error) {
-
-  console.error('MongoDB connection failed ❌:', error.message);
-}
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
+
 start();
--- 03-task-manager/starter/routes/Task.js
+++ 03-task-manager/starter/routes/Task.js
@@ -1,32 +1,17 @@
-const express = require('express');
-const router = express.Router();
 
+const express = require('express')
+const router = express.Router()
 
-// Import controller functions
 const {
-  getPeople,
-  getPerson,
-  addPerson,
-  updatePerson,
-  deletePerson
-} = require('../controllers/people.js'); // Import the controller functions
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+  editTask,
+} = require('../controllers/tasks')
 
-// Define routes and associate them with controller functions
+router.route('/').get(getAllTasks).post(createTask)
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
 
-// GET all people
-router.get('/', getPeople); // Calls the getPeople function from controller
-
-// GET person by ID
-router.get('/:id', getPerson); // Calls the getPerson function from controller
-
-// POST new person
-router.post('/', addPerson); // Calls the addPerson function from controller
-
-// PUT update person
-router.patch('/:id', updatePerson); // Calls the updatePerson function from controller
-
-// DELETE person
-router.delete('/:id', deletePerson); // Calls the deletePerson function from controller
-
-// Export the router to be used in the main app
-module.exports = router;
+module.exports = router
